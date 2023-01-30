7b90a5043cd5a2df5e7f1e218f55fd0263b88079
hitchiker-V <hitchikers-pod@Avis-MacBook-Air.local>
2023-01-31 01:18:47 +0530

    Add eventing.py

--- eventing.py
+++ eventing.py
@@ -3,6 +3,7 @@ def create_delivery(state, event):
     data = json.loads(event.data)
     
     return {
+        
         "id": event.delivery_id,
         "budget": int(data["budget"]),
         "notes": data["notes"],
