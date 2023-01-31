138f7d6e64ddd8177854ee3669a3cac840551ead
hitchiker-V <hitchikers-pod@Avis-MacBook-Air.local>
2023-02-01 01:28:08 +0530

    Add events with basic validation

--- eventing.py
+++ eventing.py
@@ -1,5 +1,7 @@
 import json
 
+from fastapi import HTTPException
+
 
 def create_delivery(state, event):
     data = json.loads(event.data)
@@ -16,12 +18,20 @@ def create_delivery(state, event):
 
 
 def start_delivery(state, event):
+
+    # Adding a basic validation to check if delivery already started
+    if state["status"] != "ready":
+        raise HTTPException(status_code=400, detail="Delivery already started")
+
     return state | {"status": "active"}
 
 
 def pickup_products(state, event):
     data = json.loads(event.data)
     new_budget = state["budget"] - int(data["purchase_price"]) * int(data["quantity"])
+
+    if new_budget < 0:
+        raise HTTPException(status_code=400, detail="Not enough budget to pickup")
 
     return state | {
         "budget": new_budget,
@@ -36,6 +46,9 @@ def deliver_products(state, event):
     new_budget = state["budget"] + int(data["sell_price"]) * int(data["quantity"])
     new_quantity = state["quantity"] - int(data["quantity"])
 
+    if new_quantity < 0:
+        raise HTTPException(status_code=400, detail="Not enought quantity")
+
     return state | {
         "budget": new_budget,
         "sell_price": int(data["sell_price"]),
@@ -44,9 +57,16 @@ def deliver_products(state, event):
     }
 
 
+def increase_budget(state, event):
+    data = json.loads(event.data)
+    state["budget"] += int(data["budget"])
+    return state
+
+
 EVENTS = {
     "CREATE_DELIVERY": create_delivery,
     "START_DELIVERY": start_delivery,
     "PICKUP_PRODUCTS": pickup_products,
     "DELIVER_PRODUCTS": deliver_products,
+    "INCREASE_BUDGET": increase_budget,
 }
